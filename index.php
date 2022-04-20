6f020d251b8e77d7fc4a2066e5563b1d3a1c88bc
Saeed <saeedre144@gmail.com>
2022-04-20 12:30:20 +0430

    commit first

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+class User{
+public $id;
+public $name;
+public $user_name;
+public $email;
+public $password;
+public $is_active;
+public $is_admin;
+
+    public function isAdmin()
+    {
+        $this->is_admin = 'true';
+    }
+
+    public function isActive()
+    {
+        $this->is_active = 'true';
+    }
+
+    public function find()
+    {
+        
+    }
+
+    public function save()
+    {
+        
+    }
+}
+
+$user = new User();
+$user->name='saeed';
+var_dump($user);
