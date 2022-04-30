60a5e1ac48fdb56a464caa1b412dc326d02d2a13
Saeed <saeedre144@gmail.com>
2022-04-30 18:37:27 +0430

    first commit

--- about.php
+++ about.php
@@ -1,3 +1,11 @@
+<?php
+
+require './../'
+
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
+require './vendor/autoload.php';
 
-class User{
-public $id;
-public $name;
-public $user_name;
-public $email;
-public $password;
-public $is_active;
-public $is_admin;
+$homeController = new \App\Controllers\HomeController();
+$homeController->index();
 
-    public function isAdmin()
-    {
-        $this->is_admin = 'true';
-    }
 
-    public function isActive()
-    {
-        $this->is_active = 'true';
-    }
 
-    public function find()
-    {
-        
-    }
 
-    public function save()
-    {
-        
-    }
-}
-
-$user = new User();
-$user->name='saeed';
-var_dump($user);
+//class User{
+//public $id;
+//public $name;
+//public $user_name;
+//public $email;
+//public $password;
+//public $is_active;
+//public $is_admin;
+//
+//    public function isAdmin()
+//    {
+//        $this->is_admin = 'true';
+//    }
+//
+//    public function isActive()
+//    {
+//        $this->is_active = 'true';
+//    }
+//
+//    public function find()
+//    {
+//
+//    }
+//
+//    public function save()
+//    {
+//
+//    }
+//}
+//
+//$user = new User();
+//$user->name='saeed';
+//var_dump($user);
